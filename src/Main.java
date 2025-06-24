090bae88018131bf45e8b51c01fe2140fe0358f0
KlueGo <klimatop777@gmail.com>
2025-06-24 19:07:37 +0300

    Loading...

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,68 @@
+import java.time.LocalDate;
 
 public class Main {
-    public static void main(String[] args) {
+
+    // Task1
+
+    public static void isLeapYear(int year) {
+        if ((year % 400 == 0) || (year % 100 != 0 && year % 4 == 0)) {
+            System.out.println(year + " год является Високосным");
+        } else {
+            System.out.println(year + " год не является Високосным");
+        }
+    }
+
+    //Task2
+
+    public static void deviceSettings (int year, int os){
+        if (year < 2015 && os == 0){
+            System.out.println("Установите облегченную версию приложения для IOS по ссылке");
+        }
+        else if (year < 2015 && os == 1){
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        }
+        else if (year >= 2015 && os == 0) {
+            System.out.println("Установите версию приложения для IOS по ссылке");
+        }
+        else if (year >= 2015 && os == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+
+        }
+    }
+
+    //Task3
+
+    public static void daysWant (int deliveryDistance){
+        if (deliveryDistance <= 20) {
+            System.out.println("Потребуется дней: 1");
+        } else if (deliveryDistance > 20 && deliveryDistance < 60) {
+            System.out.println("Потребуется дней: 2");
+        } else if (deliveryDistance >= 60 && deliveryDistance <= 100) {
+            System.out.println("Потребуется дней: 3");
+        } else {
+            System.out.println("Доставки нет.");
+        }
+    }
+
+    public static void main (String[] args) {
+
+        //Task1
+
+        isLeapYear(2019);
+
+        //Task2
+
+        int currentYear = 2004;
+        int phoneOS = 0;
+
+        deviceSettings(currentYear, phoneOS);
+
+        //Task3
+
+    daysWant(120);
 
     }
+
 }
+
+
